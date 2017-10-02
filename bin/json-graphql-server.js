f320b0971e3abd52f67bc0f58a2a92cb1b18255e
Yuriy_Tataryntsev <Yuriy_Tataryntsev@epam.com>
2017-10-02 13:32:29 +0300

    options request fix

--- bin/json-graphql-server.js
+++ bin/json-graphql-server.js
@@ -14,7 +14,23 @@ var data = require(path.join(process.cwd(), dataFilePath));
 var PORT = 4000;
 var app = express();
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    //intercepts OPTIONS method
+    if ('OPTIONS' === req.method) {
+        //respond with 200
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
+
 app.use('/', JsonGraphqlServer.jsonGraphqlExpress(data));
+
 app.listen(PORT);
 var msg = `GraphQL server running with your data at http://localhost:${PORT}/`;
 console.log(msg); // eslint-disable-line
